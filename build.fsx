2b565acf07926385e09aad5328838582e10e511f
TeaDrivenDev <TeaDrivenDev@gmail.com>
2020-04-08 03:22:58 +0200

    Replace raw `dotnet run` calls with FAKE API use

--- build.fsx
+++ build.fsx
@@ -1,5 +1,6 @@
 #r "paket:
 nuget Fake.Core.Target
+nuget Fake.DotNet.Cli
 nuget Fake.IO.FileSystem
 //"
 
@@ -9,35 +10,18 @@ open System.IO
 
 open Fake.Core
 open Fake.Core.TargetOperators
+open Fake.DotNet
 open Fake.IO
 open Fake.IO.FileSystemOperators
 
 let cwd = __SOURCE_DIRECTORY__
-let dotnet = "dotnet"
 let projectPath = cwd </> "LiteDB.FSharp"
 let testsPath = cwd </> "LiteDB.FSharp.Tests"
 
-let run workingDir fileName args =
-    printfn "CWD: %s" workingDir
-    let fileName, args =
-        if Environment.isUnix
-        then fileName, args
-        else "cmd", ("/C " + fileName + " " + args)
-    let exitCode =
-        { 
-            Program = fileName
-            WorkingDir = workingDir
-            CommandLine = args
-            Args = []
-        }
-        |> Fake.Core.Process.shellExec
-
-    if exitCode <> 0 then
-        failwithf "'%s> %s %s' task failed" workingDir fileName args
-
 Target.create "RunTests" <| fun _ ->
-    "run LiteDB.FSharp.Tests.fsproj"
-    |> run testsPath dotnet
+    "LiteDB.FSharp.Tests.fsproj"
+    |> DotNet.exec (fun defaults -> { defaults with WorkingDirectory = testsPath}) "run"
+    |> ignore
 
 Target.create "Clean" <| fun _ -> 
     [
@@ -49,23 +33,46 @@ Target.create "Clean" <| fun _ ->
     |> Shell.cleanDirs
 
 Target.create "Build" <| fun _ ->
-    "build -c Release LiteDB.FSharp.fsproj"
-    |> run projectPath dotnet 
+    let setParams (defaults: DotNet.BuildOptions) =
+        {
+            defaults with
+                Configuration = DotNet.BuildConfiguration.Release
+        }
+
+    DotNet.build setParams (projectPath </> "LiteDB.FSharp.fsproj")
+
+Target.create "PackNuget" <| fun _ ->
+    let setParams (defaults: DotNet.PackOptions) =
+        {
+            defaults with
+                Configuration = DotNet.BuildConfiguration.Release
+        }
+
+    DotNet.pack setParams projectPath
 
 Target.create "PublishNuget" <| fun _ ->
-    let projectPath = cwd </> "LiteDB.FSharp"
-    "pack -c Release"
-    |> run projectPath dotnet 
     let nugetKey =
         match Environment.environVarOrNone "NUGET_KEY" with
         | Some nugetKey -> nugetKey
         | None -> failwith "The Nuget API key must be set in a NUGET_KEY environmental variable"
     let nupkg = Directory.GetFiles(projectPath </> "bin" </> "Release") |> Seq.head
-    let pushCmd = sprintf "nuget push %s -s nuget.org -k %s" nupkg nugetKey
-    run projectPath dotnet pushCmd
+
+    let setParams (defaults: DotNet.NuGetPushOptions) =
+        {
+            defaults with
+                PushParams =
+                    {
+                        defaults.PushParams with
+                            ApiKey = Some nugetKey
+                            Source = Some "nuget.org"
+                    }
+        }
+
+    DotNet.nugetPush setParams nupkg
 
 "Clean"
    ==> "Build"
+   ==> "PackNuget"
    ==> "PublishNuget"
 
 "Clean"
